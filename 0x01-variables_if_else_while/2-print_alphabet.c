7556ade38c9ea752bd20ddf66b06996301bd9cf6
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-03 13:00:39 -0700

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(aiphabet);
+	}
+	putchar('\n');
+	return (0);
+}
