88555b8ae60ff88e9ec36160de1f04f63a9ce36e
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-07 18:59:05 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-printf_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - printnumbers to 98
+ * @n: input number
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
