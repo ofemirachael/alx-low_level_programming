cf72e56aab9d64c01ba61c0495a147765a66c7f8
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-07 18:33:14 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ */
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
