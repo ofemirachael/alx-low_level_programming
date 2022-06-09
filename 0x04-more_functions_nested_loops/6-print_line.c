f795a0ca05d4000544e5ca6e1e07ac08d8ab54e8
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-09 16:56:48 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print lines
+ * @n: number of lines
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
