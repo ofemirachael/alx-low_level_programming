97cf94c4650e671ecf9f8d909b1607d9480c0598
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-13 15:25:21 -0700

    key gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Function to randomly generates password
+ * Return: 0
+ */
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
