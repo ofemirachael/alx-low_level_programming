7247449814ac60f8a71526e69ac177af6071928c
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-23 17:39:42 -0700

    print recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry point
+ * @s: input string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s	!= '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
