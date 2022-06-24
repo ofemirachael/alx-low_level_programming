d104a248ee584e1fca7df0a76c2c48d7c9bfe4a4
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-23 17:29:42 -0700

    first recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entrypoint to print a string
+ * @s: String to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+		_putchar('\n');
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+int _putchar(char c);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int wrapper(int n, int guess);
+int is_prime_number(int n);
+int divide_number(int n, int guess);
+int _strlen_recursion(char *s);
+int count_letters(int i, char *s);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
