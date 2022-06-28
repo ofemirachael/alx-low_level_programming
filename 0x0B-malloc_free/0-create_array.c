88725c7e0491cd72a7d74ee4692b48e80eb8d1ad
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-27 20:24:12 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *create_array - Entry point
+  *@size: size of array.
+  *@c: char to initialize array with.
+  *
+  *Return: NULL or Pointer to array.
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(char) * size);
+
+	/* check if malloc was successful */
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+
+}
