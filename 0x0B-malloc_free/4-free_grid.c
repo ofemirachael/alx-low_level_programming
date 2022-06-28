26509f62c87937491110adf56990a3c8cde86233
Oluwafemi Olayode <ofemiracheal95@gmail.com>
2022-06-27 20:41:27 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - enrty point
+ * @grid: 2 dim. array to free
+ * @height: input height (size)
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
